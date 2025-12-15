fb33f582a350bad7ece1910c8bc93d3baad0fd3e
Morteza Malvandi <malvandi.m@gmail.com>
2025-12-15 08:42:33 +0330

    Initialize project

---
+++ src/test/kotlin/ir/snapppay/billing/TestBillingApplication.kt
@@ -0,0 +1,9 @@
+package ir.snapppay.billing
+
+import org.springframework.boot.fromApplication
+import org.springframework.boot.with
+
+
+fun main(args: Array<String>) {
+	fromApplication<Application>().with(TestcontainersConfiguration::class).run(*args)
+}
